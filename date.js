418ea1468ed8ad2b28bfd5c5ac583309da87745d
calebchris000 <calebchris000@gmail.com>
2023-02-17 09:17:32 +0100

    Make time 12 hours, add am/pm

--- date.js
+++ date.js
@@ -17,12 +17,12 @@ const monthNames = [
 
 function countTime() {
   let newDate = new Date();
-
   let month = newDate.getMonth();
   let day = newDate.getDay();
   let year = newDate.getFullYear();
-  let time = `${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}`;
-  dateWrapper.innerHTML = `${monthNames[month]} ${day +12} ${year}, ${time}`;
+  let ampm =  newDate.getHours() >=12 ? 'pm' : 'am'
+  let time = `${newDate.toLocaleTimeString()}`;
+  dateWrapper.innerHTML = `${monthNames[month]} ${day +12} ${year}, ${time} ${ampm}`;
 }
 
 setInterval(countTime, 1000);
